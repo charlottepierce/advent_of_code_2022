251c720005a3c84251d3dfe0646efbd6bcb838a8
Charlotte Pierce <charlotte@malformed-bits.com>
2022-12-08 14:32:28 +1100

    Finish day 2 part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_02/main.py
+++ day_02/main.py
@@ -15,13 +15,23 @@ class Outcomes(Enum):
     LOSS = 0
 
 
-STRATEGY_GUIDE_CODES = {
+STRATEGY_GUIDE_CODES_PT1 = {
     "A" : Moves.ROCK,
     "B" : Moves.PAPER,
     "C" : Moves.SCISSORS,
     "X" : Moves.ROCK,
     "Y" : Moves.PAPER,
     "Z" : Moves.SCISSORS
+}
+
+
+STRATEGY_GUIDE_CODES_PT2 = {
+    "A" : Moves.ROCK,
+    "B" : Moves.PAPER,
+    "C" : Moves.SCISSORS,
+    "X" : Outcomes.LOSS,
+    "Y" : Outcomes.DRAW,
+    "Z" : Outcomes.WIN
 }
 
 
@@ -37,13 +47,32 @@ def _game_outcome(opponent_move, move):
     return Outcomes.LOSS
 
 
+def _find_move(opponent_move, expected_outcome):
+    for move in Moves:
+        if _game_outcome(opponent_move, move) is expected_outcome:
+            return move
+
+
 if __name__ == "__main__":
+    # part 1
     score = 0
     with open("input.txt", 'r') as f:
         for line in f:
-            opponent_move, my_move = [STRATEGY_GUIDE_CODES[m] for m in line.strip().split(" ")]
+            opponent_move, my_move = [STRATEGY_GUIDE_CODES_PT1[m] for m in line.strip().split(" ")]
 
             score += my_move.value
             score += _game_outcome(opponent_move, my_move).value
 
     print("Part 1:", score)
+
+    # part 2
+    score = 0
+    with open("input.txt", 'r') as f:
+        for line in f:
+            opponent_move, expected_outcome = [STRATEGY_GUIDE_CODES_PT2[m] for m in line.strip().split(" ")]
+
+            my_move = _find_move(opponent_move, expected_outcome)
+            score += my_move.value
+            score += expected_outcome.value
+
+    print("Part 2:", score)
