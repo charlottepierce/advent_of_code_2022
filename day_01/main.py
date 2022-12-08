14ba0232ce6087254bad7e649a7ef52f7e769283
Charlotte Pierce <charlotte@malformed-bits.com>
2022-12-08 13:54:33 +1100

    Finish day 1 part 1.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

---
+++ day_01/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    calories_per_elf = []
+    with open("input.txt", 'r') as f:
+        calorie_count = 0
+        for line in f:
+            if line.strip() == "":
+                calories_per_elf.append(calorie_count)
+                calorie_count = 0
+            else:
+                calorie_count += int(line.strip())
+
+    # part 1: max calories held by one elf
+    print("Part 1:", max(calories_per_elf))
