13050c5444cd7b38ee2a6353cb33ee5ad1c5fa4c
Charlotte Pierce <charlotte@malformed-bits.com>
2022-12-11 12:36:03 +1100

    Finish day 8 part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_08/main.py
+++ day_08/main.py
@@ -3,7 +3,6 @@
 def _blocked_vertically(tree_height, tree_grid, row, col_start, col_end):
     for test_col in range(col_start, col_end):
         if tree_grid[row][test_col] >= tree_height:
-            # print(f" -- Blocked vertically by tree with height {tree_grid[row][test_col]} at ({row},{test_col})")
             return True
     return False
 
@@ -11,7 +10,6 @@ def _blocked_vertically(tree_height, tree_grid, row, col_start, col_end):
 def _blocked_horizontally(tree_height, tree_grid, col, row_start, row_end):
     for test_row in range(row_start, row_end):
         if tree_grid[test_row][col] >= tree_height:
-            # print(f" -- Blocked horizontally by tree with height ({tree_grid[test_row][col]}) at ({test_row},{col})")
             return True
     return False
 
@@ -24,7 +22,6 @@ def part1(tree_grid):
                 num_visible += 1 # tree is on the outside, always going to be visible
             else:
                 tree_height = tree_grid[row][col]
-                # print(f"Testing tree at ({row},{col}) with height {tree_height}")
                 visible = (
                     not _blocked_vertically(tree_height, tree_grid, row, 0, col) or # check up
                     not _blocked_vertically(tree_height, tree_grid, row, col + 1, len(tree_grid[0])) or # check down
@@ -36,11 +33,38 @@ def part1(tree_grid):
     return num_visible
 
 
+def _viewing_distance(tree_grid, row, col, row_start, row_end, col_start, col_end, hstep=1, vstep=1):
+    tree_height = tree_grid[row][col]
+    distance = 0
+    for test_row in range(row_start, row_end, hstep):
+        for test_col in range(col_start, col_end, vstep):
+            if tree_grid[test_row][test_col] >= tree_height:
+                distance += 1
+                return distance
+            distance += 1
+    return distance
+
+
+def part2(tree_grid):
+    highest_scenic_score = 0
+    for row in range(len(tree_grid)):
+        for col in range(len(tree_grid[0])):
+            scenic_score = 1
+            scenic_score *= _viewing_distance(tree_grid, row, col, row, row+1, col - 1, -1, vstep=-1) # up
+            scenic_score *= _viewing_distance(tree_grid, row, col, row, row+1, col + 1, len(tree_grid[0])) # down
+            scenic_score *= _viewing_distance(tree_grid, row, col, row - 1, -1, col, col + 1, hstep=-1) # left
+            scenic_score *= _viewing_distance(tree_grid, row, col, row + 1, len(tree_grid), col, col + 1) # right
+            if scenic_score > highest_scenic_score:
+                highest_scenic_score = scenic_score
+
+    return highest_scenic_score    
+
+
 if __name__ == "__main__":
     tree_grid = []
     with open("input.txt", 'r') as f:
         for line in f:
             tree_grid.append([int(x) for x in line.strip()])
 
-    # print(tree_grid)
     print(f"Part 1: {part1(tree_grid)}")
+    print(f"Part 2: {part2(tree_grid)}")
