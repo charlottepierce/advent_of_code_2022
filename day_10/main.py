8410be639fd5d3e3ce9e3b281da00ba6094ac85e
Charlotte Pierce <charlotte@malformed-bits.com>
2022-12-11 14:51:57 +1100

    Finish day 10 part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_10/main.py
+++ day_10/main.py
@@ -23,6 +23,45 @@ def part1(commands):
 
     return signal_sum
 
+
+def part2(commands):
+    cycle_num = 0
+    x = 1 # represents the middle of a 3 pixel-wide sprite
+
+    pixels = []
+    drawing_at = 0
+
+    while len(commands) > 0:
+        operation, amount = commands.pop(0)
+        cycle_num += 1
+
+        # draw pixel
+        sprite_pixels = [x - 1, x, x + 1]
+        if drawing_at in sprite_pixels:
+            pixels.append("#")
+        else:
+            pixels.append(".")
+        drawing_at += 1
+        if drawing_at > 39:
+            drawing_at = 0
+
+        if operation == "addx":
+            cycle_num += 1
+            # draw pixel
+            sprite_pixels = [x - 1, x, x + 1]
+            if drawing_at in sprite_pixels:
+                pixels.append("#")
+            else:
+                pixels.append(".")
+            drawing_at += 1
+            if drawing_at > 39:
+                drawing_at = 0
+
+            x += amount
+
+    return pixels
+
+
 if __name__ == "__main__":
     commands = []
     with open("input.txt", 'r') as f:
@@ -33,4 +72,10 @@ if __name__ == "__main__":
             else:
                 commands.append((elements[0], int(elements[1])))
 
-    print(f"Part 1: {part1(commands)}")
+    print(f"Part 1: {part1(commands[:])}")
+    print(f"Part 2")
+    pixels = part2(commands[:])
+    for i in range(len(pixels)):
+        print(pixels[i], end="")
+        if (i + 1) % 40 == 0:
+            print()
