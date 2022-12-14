1b628d62bb19a31f4979f0dc47de94060cd542df
Charlotte Pierce <charlotte@malformed-bits.com>
2022-12-14 13:54:21 +1100

    Finish day 13 part 2.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- day_13/main.py
+++ day_13/main.py
@@ -2,17 +2,13 @@
 import ast
 
 def compare(left, right):
-    print(f" - Compare {left} vs {right}")
     # If both values are integers, the lower integer should come first.
     if isinstance(left, int) and isinstance(right, int):
         if left < right:
-            print("    - both ints, left is lower -- they are in order")
             return True # If the left integer is lower than the right integer, the inputs are in the right order
         elif left > right:
-            print("    - both ints, left is higher -- they are NOT in order")
             return False # If the left integer is higher than the right integer, the inputs are not in the right order.
         else:
-            print("    - both ints, both the same -- move on")
             return None
 
     # Otherwise, the inputs are the same integer; continue checking the next part of the input.
@@ -20,13 +16,11 @@ def compare(left, right):
         for i in range(len(left)):
             # If the right list runs out of items first, the inputs are not in the right order.
             if i >= len(right):
-                print("    - both lists, right ran out of items first -- they are NOT in order")
                 return False
 
             # If both values are lists, compare the first value of each list, then the second value, and so on.
             result = compare(left[i], right[i])
             if result == False:
-                print(f"    - both lists, comparing items {left[i]} and {right[i]} failed -- they are NOT in order")
                 return False
             elif result == True:
                 return True
@@ -34,7 +28,6 @@ def compare(left, right):
 
         # If the left list runs out of items first, the inputs are in the right order
         if len(left) < len(right):
-            print("    - both lists, left ran out of items first -- they are in order")
             return True
 
     # If the lists are the same length and no comparison makes a decision about the order (or both are not lists), continue checking the next part of the input.
@@ -48,16 +41,34 @@ def compare(left, right):
 def part1(pairs):
     sum = 0
     for i in range(len(pairs)):
-        print(f"Pair {i + 1}")
         left, right = pairs[i]
         if compare(left, right):
-            print("YEP")
             sum += i + 1
-        else:
-            print("NOPE")
 
     return sum
 
+
+def part2(pairs):
+    packets = [i for pair in pairs for i in pair]
+    packets.append([[2]])
+    packets.append([[6]])
+
+    # run bubble sort
+    n = len(packets)
+    while True: # dammit python, no repeat until loop
+        swapped = False
+
+        for i in range(1, n):
+            if not compare(packets[i - 1], packets[i]):
+                packets[i - 1], packets[i] = packets[i], packets[i - 1]
+                swapped = True
+
+        if not swapped:
+            break
+    
+    # find indices of decoder packets and multiply for answer
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
+    
 
 if __name__ == "__main__":
     lines = []
@@ -72,3 +83,4 @@ if __name__ == "__main__":
         pairs.append((item1, item2))
 
     print(f"Part 1: {part1(pairs)}")
+    print(f"Part 2: {part2(pairs)}")
